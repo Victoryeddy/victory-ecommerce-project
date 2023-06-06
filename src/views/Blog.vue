a5c85e86d7d567c65ded7273f7a2dfff91777051
Victory Ndu <victor5ndu@gmail.com>
2023-06-06 07:15:25 +0100

    Add news article endpoint to display articles

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -23,37 +23,99 @@
 
     <main class="main-section">
       <v-container>
-        <v-row justify="center" align="center">
-         <v-col cols="12" lg="7">
-          <div v-for="article in allArticles" :key="article.title">
+        <v-row>
+          <v-col cols="12" lg="8">
+            <div v-if="isLoading" class="d-flex" align="center" justify="center"  >
+             <v-img src="@/assets/802.gif" width="100" height="200"></v-img>
+            </div>
+            <div v-else>
 
-            <v-card class="mt-5 border-dark fashion-items-card" >
-              <v-img :src="article.image" cover>
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-progress-circular
-                      indeterminate
-                      color="grey-lighten-5"
-                    ></v-progress-circular>
-                  </v-row>
-                </template>
-              </v-img>
-
-              <div class="card-overlay"></div>
-            </v-card>
-            <p class="font-weight-medium mt-4">
-             {{ article.title }}
-            </p>
-            <p class="font-xs mt-1">By Nancy Ward on {{ getFormattedDate(article.publishedAt) }}</p>
-            <p class="font-xs mt-2">
-             {{ article.content }}
-            </p>
-            <router-link :to="article.url" class="fs-3">Read More</router-link>
-          </div>
+              <div v-for="article in allArticles" :key="article.title">
+                <v-card class="mt-5 border-dark fashion-items-card">
+                  <v-img :src="article.image" cover>
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="grey-lighten-5"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+  
+                  <div class="card-overlay"></div>
+                </v-card>
+                <p class="font-weight-medium mt-4">
+                  {{ article.title }}
+                </p>
+                <p class="font-xs mt-1">
+                  By Nancy Ward on {{ getFormattedDate(article.publishedAt) }}
+                </p>
+                <p class="font-xs mt-2">
+                  {{ article.content }}
+                </p>
+                <a :href="article.url" class="fs-3">Read More</a>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="12" lg="4">
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <p class="blog-category-heading font-weight-bold">
+                      Category
+                    </p>
+                    <v-list lines="one">
+                      <v-list-item class="border-top"
+                        ><v-btn
+                          variant="plain"
+                          @click="getFilteredData('general')"
+                        >
+                          General
+                        </v-btn></v-list-item
+                      >
+                      <v-list-item class="border-top"
+                        ><v-btn
+                          variant="plain"
+                          @click="getFilteredData('business')"
+                        >
+                          Business
+                        </v-btn></v-list-item
+                      >
+                      <v-list-item class="border-top"
+                        ><v-btn
+                          variant="plain"
+                          @click="getFilteredData('sports')"
+                        >
+                          Sports
+                        </v-btn></v-list-item
+                      >
+                      <v-list-item class="border-top"
+                        ><v-btn
+                          variant="plain"
+                          @click="getFilteredData('science')"
+                        >
+                          Science
+                        </v-btn></v-list-item
+                      >
+                      <v-list-item class="border-bottom border-top"
+                        ><v-btn
+                          variant="plain"
+                          @click="getFilteredData('health')"
+                        >
+                          Health
+                        </v-btn></v-list-item
+                      >
+                    </v-list>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
           </v-col>
         </v-row>
       </v-container>
@@ -65,30 +127,49 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue"
 import Footer from "@/components/Footer.vue"
-import { getFormattedDate } from '@/utilities';
+import { getFormattedDate } from "@/utilities"
 
 //  using lifecycle hooks
 import { onMounted, ref, reactive } from "vue"
 import apiClient from "@/plugins/articlesAxiosFile"
 
+const isLoading = ref(true)
 const allArticles = ref([])
-
+// const searchFilteredArticles = ref([])
 
 function getArticles() {
   apiClient
     .get(
-      `top-headlines?category=general&lang=en&country=us&max=8&apikey=${process.env.VUE_APP_NEWS_API_KEY}`
+      `top-headlines?category=general&lang=en&country=us&max=4&apikey=${process.env.VUE_APP_NEWS_API_KEY}`
     )
     .then((response) => {
       allArticles.value = response.data.articles
-      console.log(allArticles.value,12345)
+      console.log(allArticles.value, 12345)
+      isLoading.value = false
     })
     .catch((error) => {
       console.log(error)
     })
 }
+
+const getFilteredData = async (category) => {
+  try {
+    const response = await apiClient.get(
+      `top-headlines?category=${category}&lang=en&country=us&max=4&apikey=${process.env.VUE_APP_NEWS_API_KEY}`
+    )
+    const data = await response.data.articles
+    allArticles.value = data
+      isLoading.value = false
+    
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 onMounted(() => {
-  console.log(process.env.VUE_APP_NEWS_API_KEY)
+  // console.log(process.env.VUE_APP_NEWS_API_KEY)
   getArticles()
 })
 </script>
@@ -96,7 +177,7 @@ onMounted(() => {
 <style>
 * {
   font-family: "Montserrat", sans-serif;
-  /* outline: 1px solid red; */
+  outline: 1px solid red;
 }
 .blog-header {
   position: absolute;
@@ -106,6 +187,17 @@ onMounted(() => {
   color: #fff;
 
   font-size: 3.5rem;
+}
+
+.blog-category-heading {
+  font-size: 1.5rem;
+  margin-left: 0.8rem;
+}
+.border-top {
+  border-top: 1px solid rgba(128, 128, 128, 0.301);
+}
+.border-bottom {
+  border-bottom: 1px solid rgba(128, 128, 128, 0.301);
 }
 
 @media only screen and (max-width: 768px) {
